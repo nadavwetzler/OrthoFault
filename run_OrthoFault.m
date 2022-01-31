390e55d9b8cf9cee4fbfb744826e28587bee2e9c
Nadav Wetzler <88764899+nadavwetzler@users.noreply.github.com>
2022-01-31 08:01:50 +0200

    Update run_OrthoFault.m

--- run_OrthoFault.m
+++ run_OrthoFault.m
@@ -13,21 +13,18 @@ cd('data')
 % filename = 'KTB-amphibolite.txt'; YM = 95000;PM = 0.26; % (1) Source: 11 
 % filename = 'Yuubari-shale.txt'; YM = 15000;PM = 0.2;   % (2) Source: 11
 % filename = 'Dunham-dolomite.txt'; YM = 111000;PM = 0.22; % (3) Source: 11
-% filename = 'Mount-Scott granite.txt'; YM = 78000; PM = 0.22; % (4) Source: 24
-filename = 'Shirahama-sandstone.txt';YM = 11500;PM = 0.38; % (5) Source: 11
+filename = 'Mount-Scott granite.txt'; YM = 78000; PM = 0.22; % (4) Source: 24
+% filename = 'Shirahama-sandstone.txt';YM = 11500;PM = 0.38; % (5) Source: 11
 % filename = 'Solenhofen-limestone.txt'; YM = 72500;PM = 0.34; % (6) Source: 11
 % filename = 'Westerly-granite-3D.txt'; YM = 70000;PM = 0.2; % (7) Source: 23 
-% filename = 'Westerly-granite-triaxial.txt'; YM = 70000;PM = 0.2; % (8) Source: 21
-% filename = 'Westerly-granite-extension.txt'; YM = 70000;PM = 0.2; % (9) Source: 21
-% filename = 'Maha-Sarakham-salt.txt'; YM = 22200;PM = 0.37; % (10) Source: 26
-% filename = 'Berea-sandstone.txt'; YM = 11000;PM = 0.2; % (11) Source: 22
-% filename = 'Tautona-quartzite.txt'; YM = 76800; PM = 0.21; % (12) Source: 27
-% filename = 'Carrara-marble.txt'; YM = 40000;PM = 0.28; % (13) Source: 25 
-% filename = 'Stripa granite.txt'; YM = 84000;PM = 0.22; % (14) Soure: 28
+% filename = 'Maha-Sarakham-salt.txt'; YM = 22200;PM = 0.37; % (8) Source: 26
+% filename = 'Berea-sandstone.txt'; YM = 11000;PM = 0.2; % (9) Source: 22
+% filename = 'Stripa granite.txt'; YM = 84000;PM = 0.22; % (10) Soure: 28
+% filename = 'Uniaxial.txt'; YM = 40000; PM = 0.22; % (12) Source: 49
 
 
-%% Selected a plane 
-ip = 4;
+
+
 
 %% Load the stress file
 DAT = importfiledata(filename);
@@ -36,5 +33,7 @@ DAT = importfiledata(filename);
 DAT = OrthoFault(DAT,PM,YM);
 
 %% Plot result
+ip = 8; % Selected a plane 
 PlotResults3DStrainA(DAT,ip)
+
 
